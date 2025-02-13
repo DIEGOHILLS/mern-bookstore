a853b58d591360c1a250b2a0e4c248c338d5159d
Diego Hills <hillsdiego33@gmail.com>
2025-02-13 15:35:05 +0200

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://diegohills:hills@mern-bookstore.m5bev.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN-bookstore';
